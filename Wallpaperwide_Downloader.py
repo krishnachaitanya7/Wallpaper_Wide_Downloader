f2d3273462fd190c74ef6b1919dbb6b737b55f21
neveisa <chaitanyaradon89@gmail.com>
2017-07-27 13:00:25 +0530

    added file checker functionality in main code

--- Wallpaperwide_Downloader.py
+++ Wallpaperwide_Downloader.py
@@ -2,6 +2,7 @@ import urllib2
 from bs4 import BeautifulSoup
 import Downloader
 import Constant_Values
+import file_checker
 type_to_download = Constant_Values.type_to_download
 first_index_of_wallpapers = Constant_Values.first_index_of_wallpapers
 last_index_of_wallpapers = Constant_Values.last_index_of_wallpapers
@@ -14,4 +15,5 @@ for i in range(first_index_of_wallpapers,last_index_of_wallpapers+1):
         if j < 10:
             print url_tag
             Downloader.download_url('http://wallpaperswide.com/download/'+url_tag.split('/')[-1][:-7]+'-wallpaper-1366x768.jpg')
+file_checker.file_checker_and_deleter()
 
--- file_checker.py
+++ file_checker.py
@@ -1,10 +1,11 @@
 import magic
 import glob
 import os
-mime = magic.Magic(mime=True)
-for file_jpg in glob.glob("*.jpg"):
-        if mime.from_file(str(file_jpg)) == 'text/html':
-		os.remove(str(file_jpg))
+def file_checker_and_deleter():
+	mime = magic.Magic(mime=True)
+	for file_jpg in glob.glob("*.jpg"):
+		if mime.from_file(str(file_jpg)) == 'text/html':
+			os.remove(str(file_jpg))
         	
         	
 	
